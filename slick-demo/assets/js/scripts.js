3533fff40d282f9eeee40883077ed481085b4296
Divija Vijay <divijavijay@gmail.com>
2026-05-07 17:23:28 +0000

    edits

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -8,8 +8,27 @@ $(document).ready(function() {
       slide: 'div',
       cssEase: 'linear',
       autoplay: true,
-      autoplaySpeed: 2000
+      autoplaySpeed: 500
     });
 
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav',
+      instructionsText: 'Changing this current slide of this carousel will change the current slide of the thumbnail carousel that follows.',
+      regionLabel: 'main image carousel'
+    });
+
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      instructionsText: 'Changing the current slide of this carousel will change the current slide of the preceding main image carousel.',
+      regionLabel: 'thumbnail carousel'
+    });
 
 });
