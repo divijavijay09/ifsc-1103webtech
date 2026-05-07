6c4bac1955d5aa22917668a466c9b9391978eacb
Divija Vijay <divijavijay@gmail.com>
2026-05-07 17:46:09 +0000

    edits

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -1,34 +1,14 @@
 $(document).ready(function() {
 
-    // $('.fade').slick({
-    //   dots: true,
-    //   infinite: true,
-    //   speed: 500,
-    //   fade: true,
-    //   slide: 'div',
-    //   cssEase: 'linear',
-    //   autoplay: true,
-    //   autoplaySpeed: 500
-    // });
-
-    $('.slider-for').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slider-nav',
-      instructionsText: 'Changing this current slide of this carousel will change the current slide of the thumbnail carousel that follows.',
-      regionLabel: 'main image carousel'
-    });
-
-    $('.slider-nav').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
+    $('.fade').slick({
       dots: true,
-      centerMode: true,
-      instructionsText: 'Changing the current slide of this carousel will change the current slide of the preceding main image carousel.',
-      regionLabel: 'thumbnail carousel'
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 500
     });
 
 });
